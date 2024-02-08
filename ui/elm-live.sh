0d0d60245863ee0d48c8815bb8504f29f45242ec
Max Brauer <max.brauer@byte-physics.de>
2024-02-08 21:00:01 +0100

    Debug: Add fix for git bash

--- ui/elm-live.sh
+++ ui/elm-live.sh
@@ -2,7 +2,7 @@
 
 # ONLY USED FOR DEVELOPMENT. You won't need it in runtime
 
-elm-live src/Main.elm \
+MSYS_NO_PATHCONV=1 elm-live src/Main.elm \
     --port 8011 \
     --proxy-prefix "/api" \
     --proxy-host "http://127.0.0.1:8000" \
