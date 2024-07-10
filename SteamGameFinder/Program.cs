56a4986aa50474240c46f2737e2d182dffc09dc3
Max Brauer <ma.brauer@live.de>
2024-07-10 14:35:55 +0200

    add cors (temporary)

--- SteamGameFinder/Program.cs
+++ SteamGameFinder/Program.cs
@@ -32,6 +32,7 @@ namespace SteamGameFinder
 
             var server = new Server(new WebServerSettings(8000, 5000));
             server.InitialDefault();
+            server.AddWebService(new CorsService());
 
             var ws = new MaxLib.WebServer.WebSocket.WebSocketService();
             ws.Endpoints.Add(new Web.WebSocketEndpoint());
@@ -91,6 +92,31 @@ namespace SteamGameFinder
                     new LogEventProperty("info", new ScalarValue(e.LogItem.Information))
                 }
             ));
+        }
+    }
+
+    public class CorsService : WebService
+    {
+        public CorsService() 
+            : base(ServerStage.CreateResponse)
+        {
+        }
+
+        public override bool CanWorkWith(WebProgressTask task)
+        {
+            return true;
+        }
+
+        public override Task ProgressTask(WebProgressTask task)
+        {
+            var header = task.Request.GetHeader("Origin") ?? "*";
+            task.Response.SetHeader("Access-Control-Allow-Origin", header);
+            task.Response.SetHeader("Vary", "Origin");
+            if ((header = task.Request.GetHeader("Access-Control-Request-Headers")) is not null)
+                task.Response.SetHeader("Access-Control-Allow-Headers", header);
+            if ((header = task.Request.GetHeader("Access-Control-Request-Method")) is not null)
+                task.Response.SetHeader("Access-Control-Allow-Methods", header);
+            return Task.CompletedTask;
         }
     }
 }
